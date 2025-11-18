4e89a32785ba4fa10c3449fe18a83c09cd7c8736
LauraChd <laura.chaduc@etud.univ-angers.fr>
2025-11-18 15:33:08 +0100

    Commit dernière modif pour zip

--- virtualcrm/src/main/java/fr/univangers/service/implementations/VirtualCRMServiceFactory.java
+++ virtualcrm/src/main/java/fr/univangers/service/implementations/VirtualCRMServiceFactory.java
@@ -3,7 +3,6 @@ package fr.univangers.service.implementations;
 import fr.univangers.clients.GeoLocalisationServiceClient;
 import fr.univangers.clients.InternalCRMClient;
 import fr.univangers.clients.SalesforceCRMClient;
-import fr.univangers.service.interfaces.VirtualCRMService;
 
 import java.io.IOException;
 
